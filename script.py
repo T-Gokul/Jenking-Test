bc8f41e42b576c2594a3b147d8887fd7ae7db339
Gokul <52142852+T-Gokul@users.noreply.github.com>
2023-11-22 11:32:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,28 @@
 import requests
 from datetime import datetime
 print("Testing")
-def get_token_expiration(token):
-    headers = {
-        'Authorization': f'token {token}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
 
-    response = requests.get('https://api.github.com/user', headers=headers)
+headers = {
+    'Authorization': 'token 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',
+    'Accept': 'application/vnd.github.v3+json'
+}
 
-    if response.status_code == 200:
-        token_info = response.json()
-        expires_at_str = token_info.get('updated_at')
+response = requests.get('https://api.github.com/user', headers=headers)
 
-        updated_at = datetime.strptime(expires_at_str, "%Y-%m-%dT%H:%M:%SZ")
-        now = datetime.now()
-        result = now - updated_at
-        result = result.days       
-        if result == 7:
-            print("Token is going to expire, renew it")
-        else:
-            print(f"Token will expire in {result} days")
+if response.status_code == 200:
+    token_info = response.json()
+    expires_at_str = token_info.get('updated_at')
 
+    updated_at = datetime.strptime(expires_at_str, "%Y-%m-%dT%H:%M:%SZ")
+    now = datetime.now()
+    result = now - updated_at
+    result = result.days       
+    if result == 7:
+        print("Token is going to expire, renew it")
     else:
-        return f"Error: {response.status_code}, {response.text}"
+        print(f"Token will expire in {result} days")
 
-get_token_expiration('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')
+else:
+    return f"Error: {response.status_code}, {response.text}"
+
+# get_token_expiration('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')
